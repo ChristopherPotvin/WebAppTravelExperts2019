b3b812de57042eb7549c1d12cbcaf6c2ae865979
Muhammad Sagnia <793391@ACDM.DS.SAIT.CA>
2019-01-23 16:58:40 -0700

    Customer Class
    create class for customers (getters and setters)

---
+++ TravelExpertsWebApp/Query/CustomersDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query
+{
+    [DataObject(true)]
+    public static class CustomersDB
+    {
+        //[DataObject(DataObjectMethodType.Insert)]
+        ////insert new customer record
+        //public static int AddCustomer(CustomersDB cust)
+        //{
+        //    int custID = 0;
+
+        //}
+    }
+}
