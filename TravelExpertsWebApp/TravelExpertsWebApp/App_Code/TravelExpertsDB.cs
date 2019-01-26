df93b3c52310ad030c3c003f27c01ff7ae1f5e80
Mo Sagnia <msagnia.ing@gmail.com>
2019-01-26 10:02:26 -0700

    create connection class to the DB

---
+++ TravelExpertsWebApp/TravelExpertsWebApp/App_Code/TravelExpertsDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelExpertsWebApp.App_Code
+{
+    public class TravelExpertsDB
+    {
+    }
+}
