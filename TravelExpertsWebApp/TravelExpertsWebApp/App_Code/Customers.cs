51c5558aa3524be92b71babdd04b736ed26fc731
Mo Sagnia <msagnia.ing@gmail.com>
2019-01-26 11:41:01 -0700

    CustomerDB class created. Add new customer to DB class created

--- TravelExpertsWebApp/TravelExpertsWebApp/App_Code/Customers.cs
+++ TravelExpertsWebApp/TravelExpertsWebApp/App_Code/Customers.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace Model
+namespace TravelExpertsWebApp.App_Code
 {
-    public class Class1
+    //create Customers class with all the pertinent getters & setters
+    public class Customers
     {
+        public Customers() { } //default constructor 
+
+        public int CustomerId {get; set; }
+
+        public string CustFirstName { get; set; }
+
+        public string CustLastName { get; set; }
+
+        public string CustAddress { get; set; }
+
+        public string CustCity { get; set; }
+
+        public string CustProv { get; set; }
+
+        public string CustPostal { get; set; }
+
+        public string CustCountry { get; set; }
+
+        public string CustHomePhone { get; set; }
+
+        public string CustBusPhone { get; set; }
+
+        public string CustEmail { get; set; }
+
+        public int AgentId { get; set; }
     }
 }
--- TravelExpertsWebApp/TravelExpertsWebApp/App_Code/CustomersDB.cs
+++ TravelExpertsWebApp/TravelExpertsWebApp/App_Code/CustomersDB.cs
@@ -1,21 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelExpertsWebApp.App_Code;
 
 namespace Query
 {
     [DataObject(true)]
     public static class CustomersDB
     {
-        //[DataObject(DataObjectMethodType.Insert)]
-        ////insert new customer record
-        //public static int AddCustomer(CustomersDB cust)
-        //{
-        //    int custID = 0;
+        [DataObjectMethod(DataObjectMethodType.Insert)]
+        //insert new customer record
+        public static int AddCustomer(Customers cust)
+        {
+            int custId = 0;
 
-        //}
+            //prepare connection
+            SqlConnection connection = TravelExpertsDB.GetConnection();
+
+            //prepare statement
+            string insertString = "INSERT INTO CUSTOMERS "+
+                                  "(CustFirstName, CustLastName, CustAddress, CustCity, CustProv, CustPostal, CustCountry, CustHomePhone, CustBusPhone, CustEmail) "+
+                                "values(@CustFirstName, @CustLastName, @CustAddress, @CustCity, @CustProv, @CustPostal, @CustCountry, @CustHomePhone, @CustBusPhone, @CustEmail)";
+            SqlCommand insertCommand = new SqlCommand(insertString, connection);
+            insertCommand.Parameters.AddWithValue("@CustFirstName", cust.CustFirstName);
+            insertCommand.Parameters.AddWithValue("@CustLastName", cust.CustLastName);
+            insertCommand.Parameters.AddWithValue("@CustAddress", cust.CustAddress);
+            insertCommand.Parameters.AddWithValue("@CustCity", cust.CustCity);
+            insertCommand.Parameters.AddWithValue("@CustProv", cust.CustProv);
+            insertCommand.Parameters.AddWithValue("@CustPostal", cust.CustPostal);
+            insertCommand.Parameters.AddWithValue("@CustCountry", cust.CustCountry);
+            insertCommand.Parameters.AddWithValue("@CustHomePhone", cust.CustHomePhone);
+            insertCommand.Parameters.AddWithValue("@CustBusPhone", cust.CustBusPhone);
+            insertCommand.Parameters.AddWithValue("@CustEmail", cust.CustEmail);
+
+            try
+            {
+                //open connection
+                connection.Open();
+
+                //execute the statement
+                int i = insertCommand.ExecuteNonQuery();
+                if(i == 1) //one record inserted
+                {
+                    // retrieve customer id from the added record
+                    //ident_current Returns the last identity value generated for the customers table
+                    string selectString = "select ident_current('Customers') from Customers";
+                    SqlCommand selectCommand = new SqlCommand(selectString, connection);
+                    custId = Convert.ToInt32(selectCommand.ExecuteScalar()); 
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return custId;
+        }
     }
 }
